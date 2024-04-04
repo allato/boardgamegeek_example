5d81197e6b709b77959d8ced724a2e61e8f8b9e3
Alessandro La Tona <a.latona@insideapp.it>
2024-04-04 19:01:57 +0200

    First commit

---
+++ lib/tab_demo.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class TabBarDemo extends StatefulWidget {
+  final String title;
+
+  const TabBarDemo({super.key, required this.title});
+
+  @override
+  _TabBarDemoState createState() => _TabBarDemoState();
+}
+
+class _TabBarDemoState extends State<TabBarDemo>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+  int _selectedIndex = 0;
+
+  List<Widget> list = [
+    const Tab(icon: Icon(Icons.card_travel)),
+    const Tab(icon: Icon(Icons.add_shopping_cart)),
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // Create TabController for getting the index of current tab
+    _controller = TabController(length: list.length, vsync: this);
+
+    _controller.addListener(() {
+      setState(() {
+        _selectedIndex = _controller.index;
+      });
+    });
+  }
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          bottom: TabBar(
+            onTap: (index) {
+              // Should not used it as it only called when tab options are clicked,
+              // not when user swapped
+            },
+            controller: _controller,
+            tabs: list,
+          ),
+        ),
+        body: TabBarView(
+          controller: _controller,
+          children: [
+            Center(
+              child: Text(
+                '$_selectedIndex: $_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+            Center(
+              child: Text(
+                '$_selectedIndex: $_counter',
+                style: const TextStyle(fontSize: 40),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
